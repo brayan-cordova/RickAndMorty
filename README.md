3553318d8863733ec67994919d95625c1daa1c7b
brayan-cordova <63522101+brayan-cordova@users.noreply.github.com>
2023-05-03 22:59:16 -0600

    getting characters from api

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RickAndMorty-frontend
 Web application where we consume the Rick and Morty API using Vue 3 - Composition Api and Vuex.  I will use NodeJS for the consumption of the api
+
+* Run the project using: npm run serve
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,36 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex'
 
 export default createStore({
   state: {
+    // stores all characters in general
+    characters: [],
+    // queries and filters for the api
+    charactersFilter: []
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload
+    },
+    setCharactersFilter(state, payload) {
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    // get the characters from the api
+    async getCharacters({
+      commit
+    }) {
+      try {
+        //getting characters
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
