b8bcddb9f004b7cffa3aebe3a29618a225d86965
brayan-cordova <63522101+brayan-cordova@users.noreply.github.com>
2023-05-04 20:28:43 -0600

    adding status filter

--- src/store/index.js
+++ src/store/index.js
@@ -31,6 +31,28 @@ export default createStore({
       } catch (error) {
         console.error(error)
       }
+    },
+    filterByStatus({
+      commit,
+      state
+    }, status) {
+      const filter = state.characters.filter((character) => {
+        return character.status.includes(status)
+      })
+      commit('setCharactersFilter', filter)
+    },
+    filterByName({
+      commit,
+      state
+    }, name) {
+      const formatName = name.toLowerCase()
+      const filter = state.characters.filter((character) => {
+        const characterName = character.name.toLowerCase()
+        if (characterName.includes(formatName)) {
+          return character
+        }
+      })
+      commit('setCharactersFilter', filter)
     }
   },
   modules: {}
